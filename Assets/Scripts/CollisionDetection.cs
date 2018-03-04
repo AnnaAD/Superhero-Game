fb9542f9437ad49ce9c3ce4672194f7f248a38b4
Langston Nashold <langston201@gmail.com>
2018-03-04 16:01:55 -0600

    Collision???

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-	
+	public delegate void ObstacleCollision();
+	public static event ObstacleCollision ObstacleCollisionEvent;
+
 	public void OnCollisionEnter2D (Collision2D coll)
 	{
 		if (coll.gameObject.tag.Equals ("Obstacle")) {
 			Debug.Log ("Collided!");
+			if (ObstacleCollisionEvent != null) {
+				ObstacleCollisionEvent ();
+			} else
+				Debug.Log ("null");
 		}
 	}
 }
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+	public delegate void GameOver();
+	public static event GameOver GameOverEvent;
+
+	// Use this for initialization
+	void Start () {
+		CollisionDetection.ObstacleCollisionEvent += GameOverEvent;
+		GameOverEvent += Test;
+		Debug.Log ("Start");
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// I wasn't sure how to trigger an event with another event so I used this useless method
+	public void GameOverHelper(){
+		
+		if (GameOverEvent != null)
+			GameOverEvent ();
+	}
+
+	public void Test() {
+		Debug.Log ("GameOver");
+	}
+
+		
+}
