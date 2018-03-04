1f7b4091afb46da1af06b22854e19e1ca7f4609e
Langston Nashold <langston201@gmail.com>
2018-03-04 14:53:44 -0600

    collision detection?

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -7,7 +7,7 @@ public class CollisionDetection : MonoBehaviour
 	
 	public void OnCollisionEnter2D (Collision2D coll)
 	{
-		if (coll.gameObject.tag == "Enemy")
+		if (coll.gameObject.tag.Equals( "Obstacle"))
 			Debug.Log ("Collided!");
 	}
 }
