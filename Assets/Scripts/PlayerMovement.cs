69d11b1c5c1c6501781067cbb921639a4c2d5a15
Ironbizmark <anna.arpacidusseau@gmail.com>
2018-03-04 15:00:35 -0600

    playermovement
    not too sketchy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	private float verticalControls;
+	[SerializeField] private float speed;
+	private Rigidbody2D rb;
 
 	void Awake() {
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	// Update is called once per frame
 	void Update () {
-
+		rb.AddForce(new Vector2(0f, verticalControls*speed));
+		if(Mathf.Abs(verticalControls) < 0.01) {
+			Vector2 currentVelocity = rb.velocity;
+			Vector2 oppositeForce = -currentVelocity * 2;
+			rb.AddRelativeForce(oppositeForce);
+		}
 	}
 
 	public void Move(float v) {
