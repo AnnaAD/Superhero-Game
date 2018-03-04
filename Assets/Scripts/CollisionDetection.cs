27c179ada12d4c71744f5fe6e05dc7dda00ff603
Langston Nashold <langston201@gmail.com>
2018-03-04 14:35:48 -0600

    stuff

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+	
+	public void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Enemy")
+			Debug.Log ("Collided!");
+	}
+}
