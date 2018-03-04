5c9a5ca60f5125a87af9c8fa49e1ce7514895780
Ironbizmark <anna.arpacidusseau@gmail.com>
2018-03-04 15:53:03 -0600

    Test obstacles

---
+++ Assets/Scripts/GenerateObstacles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObstacles : MonoBehaviour {
+	[SerializeField] private GameObject obstacle;
+	[SerializeField] public float speed;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0,50) == 1) {
+			GameObject instatiated = Instantiate(obstacle) as GameObject;
+			instatiated.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(1.1f,Random.Range(0.0f,1.0f)));
+			instatiated.transform.SetParent(this.transform);
+		}
+	}
+}
---
+++ Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour {
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		speed = GetComponentInParent<GenerateObstacles>().speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2(transform.position.x - (speed*Time.fixedDeltaTime), transform.position.y);
+	}
+
+	void OnBecameInvisible() {
+		Destroy(gameObject);
+	}
+}
