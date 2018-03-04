e3071eb0dc218e0fefe04ac6bba83178357f381e
Langston Nashold <langston201@gmail.com>
2018-03-04 17:31:36 -0600

    Scoring?

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+	private GameObject distanceText; 
+	// Use this for initialization
+	void Start () {
+		distanceText = GetComponent<Transform>().Find("DistanceText").gameObject; //Can also use .GetChild( int index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateDistanceText(int distance) {
+		distanceText.GetComponent<UnityEngine.UI.Text> ().text = distance + "m";
+	}
+
+}
