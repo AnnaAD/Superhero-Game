8f347b338575a06b93895931df7c3f97ed4838dd
lgnashold <lgnashold@gmail.com>
2018-08-26 19:54:09 -0500

    Hopefully more fun now

--- Assets/Scripts/GenerateObstacles.cs
+++ Assets/Scripts/GenerateObstacles.cs
@@ -7,22 +7,31 @@ public class GenerateObstacles : MonoBehaviour {
 	[SerializeField] private GameObject[] fastObstacles;
 	[SerializeField] private float timeBetweenBuildings;
 	private float countBetweenBuildings;
-	// Chance of additional flying obstacle
-	[SerializeField] private int chanceOfExtraObstacle;
-
+	// min and max values for nextFlyingTime
+	[SerializeField] private int minFlyingTime;
+	[SerializeField] private int maxFlyingTime;
+	//Time since last flying obstacle
+	private float flyingCount;
+	// Total time between last obstacle and next obstacle
+	private float nextFlyingTime;
 	//The speed of all obstacles, effectivally the horizontal speed of the player
 	[SerializeField] public float speed;
 	[SerializeField] public float acceleration;
 	// Use this for initialization
 	void Start () {
-		countBetweenBuildings = 0;
+		countBetweenBuildings = timeBetweenBuildings * 3/4;
+		flyingCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Updates speed of player
 		speed += acceleration * Time.fixedDeltaTime;
+		
 		countBetweenBuildings += Time.fixedDeltaTime;
-		if(countBetweenBuildings > timeBetweenBuildings) {
+		flyingCount += Time.fixedDeltaTime;
+		//Generates buildings
+		if(countBetweenBuildings > timeBetweenBuildings / speed) {
 			countBetweenBuildings = 0;
 			int obj = Random.Range(0,staticObstacles.Length);
 			GameObject instatiated = Instantiate(staticObstacles[obj]) as GameObject;
@@ -30,10 +39,12 @@ public class GenerateObstacles : MonoBehaviour {
 			instatiated.transform.SetParent(this.transform);
 		}
 
-		if(Random.Range(0,chanceOfExtraObstacle) == 1) {
+		// Generates Flying Objects
+		if(flyingCount > nextFlyingTime / speed) {
+			flyingCount = 0;
+			nextFlyingTime = Random.Range(minFlyingTime, maxFlyingTime);
 			int obj = Random.Range(0, fastObstacles.Length-1);
 			Debug.Log(obj);
-
 			GameObject instatiated = Instantiate(fastObstacles[obj]) as GameObject;
 			instatiated.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(1.2f,Random.Range(0.3f,1.0f)));
 			instatiated.transform.SetParent(this.transform);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,24 +9,34 @@ public class PlayerMovement : MonoBehaviour {
 	// Multiplied by the acceleration to find force slowing down player when no controls are applied
 	[SerializeField] private float decelerationFactor;
 
+	//Rigidbody and transform components of Player
 	private Rigidbody2D rb;
-
+	private Transform trans;
+	//Obstacle generation Script
+	GenerateObstacles obstScript;
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		trans = GetComponent<Transform>();
+		obstScript = GameObject.Find("Obstacles").GetComponent<GenerateObstacles>();
 	}
 	// Update is called once per frame
 	void Update () {
-		Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+		Vector2.ClampMagnitude(rb.velocity, maxSpeed * obstScript.speed);
 		//If there is no movement
-		if(Mathf.Abs(verticalControls) < 0.001) {
+		if(Mathf.Abs(verticalControls) < 0.001 || trans.position.y > 5 || trans.position.y < -5) {
 			// Takes the currentVelocity, and adds a force in the opposite direction
 			Vector2 currentVelocity = rb.velocity;
-			Vector2 oppositeForce = -currentVelocity * acceleration* decelerationFactor;
+			Vector2 oppositeForce = -currentVelocity * acceleration * obstScript.speed * decelerationFactor;
 			rb.AddForce(oppositeForce);
 		} else {
-			rb.AddForce(new Vector2(0f, verticalControls*acceleration));
+			rb.AddForce(new Vector2(0f, verticalControls*acceleration * obstScript.speed));
 		}
-		rb.MoveRotation(rb.velocity.y * 5);
+		rb.MoveRotation(rb.velocity.y * 15 / obstScript.speed);
+		if(trans.position.y > 5) {
+			trans.position = new Vector3(trans.position.x, 5, trans.position.z);
+		} else if(trans.position.y < -5) {
+			trans.position = new Vector3(trans.position.x, -5, trans.position.z);
+		}
 	}
 
 	// Called from Player Input
